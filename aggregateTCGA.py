042d4b91609c629c596053da530d539212ec90f0
Christopher L Plaisier, PhD <cplaisier@systemsbiology.org>
2019-10-15 21:36:22 -0700

    Added some plotting functionality.

--- aggregateTCGA.py
+++ aggregateTCGA.py
@@ -26,6 +26,10 @@
 import pandas as pd
 import json
 from scipy.stats import hypergeom
+from scipy.stats import pearsonr
+import seaborn as sns
+from matplotlib.backends.backend_pdf import PdfPages
+import matplotlib.pyplot as plt
 
 
 ###############
@@ -78,12 +82,67 @@ for tumor in tumors:
 # Load up gene sets for each analysis
 merged = {}
 geneSets = {}
+summaryTable = { 'no_filter':pd.DataFrame(index=tumors, columns=['PAM','CNAamp','Act','CNAdel','LoF']), 
+                 'pq':pd.DataFrame(index=tumors, columns=['PAM','CNAamp','Act','CNAdel','LoF']),
+                 'mcr':pd.DataFrame(index=tumors, columns=['PAM','CNAamp','Act','CNAdel','LoF']),
+                 'pq_mcr':pd.DataFrame(index=tumors, columns=['PAM','CNAamp','Act','CNAdel','LoF'])}
+mutFreq = pd.DataFrame(columns = ['tumor','freq'])
 for tumor in tumors:
     merged[tumor] = {}
     geneSets[tumor] = {}
     for analysis in analyses:
         merged[tumor][analysis] = pd.read_csv('TCGA/output/'+tumor+'/'+analysis+'/oncoMerge_mergedMuts.csv', header = 0, index_col = 0)
         geneSets[tumor][analysis] = [[int(j.split('_')[0]),j.split('_')[1]] for j in merged[tumor][analysis].index[[not i for i in list(merged[tumor][analysis].index.str.contains('p|q'))]]]
+        if analysis=='pq_mcr':
+            mutFreq = mutFreq.append(pd.DataFrame({'tumor':[tumor]*merged[tumor]['pq_mcr'].shape[0], 'freq':list(merged[tumor]['pq_mcr'].T.mean())}))
+        tmp1 = [i.split('_') for i in merged[tumor][analysis].index]
+        for mutType in ['PAM','CNAamp','Act','CNAdel','LoF']:
+            summaryTable[analysis].loc[tumor, mutType] = len([i for i in tmp1 if i[1]==mutType])
+
+# Write out summary table
+summaryTable['no_filter'].to_csv('summaryTable_no_filter.csv')
+summaryTable['pq'].to_csv('summaryTable_pq.csv')
+summaryTable['mcr'].to_csv('summaryTable_mcr.csv')
+summaryTable['pq_mcr'].to_csv('summaryTable_pq_mcr.csv')
+
+with PdfPages('summaryPlots_pq_mcr.pdf') as pp:
+    fig, ax = plt.subplots(2,1,figsize=(8,6)) #sharex='col'
+    # Plot final mutation counts
+    #fig, ax = plt.subplots(figsize=(8,4))
+    summaryTable['pq_mcr'].T.reindex(summaryTable['pq_mcr'].T.sum().sort_values().index, axis=1).T.plot(kind='bar', stacked=True, ax=ax[0])
+    ax[0].set_ylabel('Number of somatic mutations')
+    #fig.tight_layout()
+    #fig.savefig(pp, format='pdf')
+
+    # Plot final mutation frequencies by tumor type
+    #mutFreq = pd.DataFrame(column=['tumor','freq'])
+   
+    o1 = summaryTable['pq_mcr'].T.sum().sort_values().index
+    #fig, ax = plt.subplots(figsize=(8,4))
+    #sns.stripplot(x='tumor',y='freq', data=mutFreq, ax=ax, order=o1, size=3, jitter=True)
+    sns.swarmplot(x='tumor',y='freq', data=mutFreq, ax=ax[1], order=o1, size=2, edgecolor='gray',linewidth=0.01, dodge=True) #, palette= sns.color_palette('deep', 33))
+    ax[1].set_xticklabels(ax[1].get_xticklabels(),rotation=90)
+    ax[1].set_ylabel('Somatic mutation frequency')
+    ax[1].set_xlabel('Tumor type')
+    ax[1].set(ylim=(0,1))
+    fig.tight_layout()
+    fig.savefig(pp, format='pdf')
+    
+    # Plot total mutation counts versus number of patients
+    fig, ax = plt.subplots(figsize=(4,4))
+    comparisonTable = pd.DataFrame(index=tumors, columns=['mut_counts','num_patients'])
+    comparisonTable['mut_counts'] = summaryTable['pq_mcr'].T.sum().sort_values()
+    for tumor in tumors:
+        comparisonTable.loc[tumor,'num_patients'] = len(merged[tumor]['pq_mcr'].columns)
+    ax = sns.regplot(x='mut_counts', y='num_patients', data = comparisonTable)
+    ax.set_ylabel('Number of patients in study')
+    ax.set_xlabel('Number of somatic mutations')
+    pc1 = pearsonr(comparisonTable['mut_counts'],comparisonTable['num_patients'])
+    ax.set_title('R = '+'%.2f'%pc1[0]+'; p-value = '+'%.3f'%pc1[1])
+    for tumor in tumors:
+        ax.text(comparisonTable.loc[tumor,'mut_counts'], comparisonTable.loc[tumor,'num_patients'], tumor, horizontalalignment='center', fontsize=6)
+    fig.tight_layout()
+    fig.savefig(pp, format='pdf')
 
 
 ####################
